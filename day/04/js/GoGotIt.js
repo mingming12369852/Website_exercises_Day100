842bdec49ee11299649112a983a9498da60e7917
SangSang_na <mingming12369852@gmail.com>
2020-04-21 19:27:33 +0800

    day04

---
+++ day/04/js/GoGotIt.js
@@ -0,0 +1,21 @@
+  var runColor;
+  var time = 0;
+  var text;
+
+  var runColor = setInterval(function() {
+    Go2();
+  }, 10);
+
+  function Go2() {
+    time += 1;
+    if (time >= 360) {
+      time = 0;
+    } else {
+      text = time + "deg";
+      $('#midCanvas').css({
+        background: "linear-gradient(" + text + ",rgb(207, 138, 153),rgb(125, 167, 172))"
+      });
+    }
+
+
+  }
